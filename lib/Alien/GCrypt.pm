0f0e8a36ee1df04c748847da266c7967c9b4ec99
Alex J. G. Burzyński <ajgb@cpan.org>
2014-11-19 00:17:52 +0000

    import

---
+++ lib/Alien/GCrypt.pm
@@ -0,0 +1,33 @@
+# ABSTRACT: Install and make available libgcrypt
+
+use strict;
+use warnings;
+package Alien::GCrypt;
+
+use parent 'Alien::Base';
+
+1;
+
+__END__
+
+=head1 SYNOPSIS
+
+    use Alien::GCrypt;
+
+    my $cflags = Alien::GCrypt->cflags;
+    my $libs = Alien::GCrypt->libs;
+    
+=head1 DESCRIPTION
+
+Alien::GCrypt installs the C library C<libgcrypt>.
+
+=head1 SEE ALSO
+
+=over 4
+
+=item * L<https://www.gnu.org/software/libgcrypt/>
+
+=back
+
+=cut
+
